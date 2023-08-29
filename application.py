dbdb9725b10a09dfad7817ae232fe3b96c571b3e
Rushikesh Suryawanshi <rushikeshsuryawanshi007@gmail.com>
2023-08-29 09:43:39 +0530

    first commit

--- application.py
+++ application.py
@@ -8,6 +8,16 @@ app = Flask(__name__)
 with open('model.pkl', 'rb') as model_file:
     model = pickle.load(model_file)
 
+# Load the mappings for actors, genres, and directors
+with open('actor_mapping.pkl', 'rb') as actor_mapping_file:
+    actor_mapping = pickle.load(actor_mapping_file)
+
+with open('genre_mapping.pkl', 'rb') as genre_mapping_file:
+    genre_mapping = pickle.load(genre_mapping_file)
+
+with open('director_mapping.pkl', 'rb') as director_mapping_file:
+    director_mapping = pickle.load(director_mapping_file)
+
 # Load the list of feature names used in training
 with open('feature_names.pkl', 'rb') as feature_names_file:
     feature_names = pickle.load(feature_names_file)
@@ -30,9 +40,17 @@ def predict():
     input_df = pd.DataFrame([input_features], columns=feature_names)
 
     # Use the trained model to make a prediction
-    prediction = model.predict(input_df)
+    prediction = model.predict(input_df)[0]
 
-    return render_template('index.html', prediction_text='Predicted Rating: {:.2f}'.format(prediction[0]))
+    # Interpret numeric values using mappings
+    genre_name = genre_mapping[int(request.form['genre'])]
+    director_name = director_mapping[int(request.form['director'])]
+    actor1_name = actor_mapping['Actor 1'][int(request.form['actor1'])]
+    actor2_name = actor_mapping['Actor 2'][int(request.form['actor2'])]
+    actor3_name = actor_mapping['Actor 3'][int(request.form['actor3'])]
 
+    return render_template('index.html', prediction_text='Predicted Rating: {:.2f}'.format(prediction),
+                           genre_name=genre_name, director_name=director_name,
+                           actor1_name=actor1_name, actor2_name=actor2_name, actor3_name=actor3_name)
 if __name__ == '__main__':
     app.run(debug=True)
--- mapping.py
+++ mapping.py
@@ -1,21 +1,34 @@
-from sklearn.preprocessing import LabelEncoder
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
+from train_model import actor_columns
 from sklearn.ensemble import RandomForestRegressor
-from train_model import data , actor_columns
+import pickle
 
+# Load the dataset
+data = pd.read_csv('IMDb Movies India.csv', encoding='iso-8859-1')
 
-# Assuming data is the loaded dataset
-label_encoder = LabelEncoder()
-data['Genre'] = label_encoder.fit_transform(data['Genre'])
+# ... (previous preprocessing code)
 
-genre_mapping = dict(zip(label_encoder.classes_, label_encoder.transform(label_encoder.classes_)))
-# print(genre_mapping)
+# Create mappings for genres, directors, and actors
+genre_mapping = {genre: idx for idx, genre in enumerate(data['Genre'].unique())}
+director_mapping = {director: idx for idx, director in enumerate(data['Director'].unique())}
+
 actor_mapping = {}
 for col in actor_columns:
-    label_encoder = LabelEncoder()
-    data[col] = label_encoder.fit_transform(data[col])
-    actor_mapping[col] = dict(zip(label_encoder.classes_, label_encoder.transform(label_encoder.classes_)))
+    actor_mapping[col] = {actor: idx for idx, actor in enumerate(data[col].unique())}
 
-print(actor_mapping)
+# Save the mappings as CSV files
+genre_mapping_df = pd.DataFrame(list(genre_mapping.items()), columns=['Genre', 'NumericValue'])
+genre_mapping_df.to_csv('genre_mapping.csv', index=False)
+
+director_mapping_df = pd.DataFrame(list(director_mapping.items()), columns=['Director', 'NumericValue'])
+director_mapping_df.to_csv('director_mapping.csv', index=False)
+
+actor_mapping_dfs = []
+for col in actor_columns:
+    actor_mapping_dfs.append(pd.DataFrame(list(actor_mapping[col].items()), columns=[col, 'NumericValue']))
+actor_mapping_combined_df = pd.concat(actor_mapping_dfs, axis=1)
+actor_mapping_combined_df.to_csv('actor_mapping.csv', index=False)
+
+# ... (continue with the rest of your preprocessing and training code)
